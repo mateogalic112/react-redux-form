9d77f181a83ff280ba08f2b4cd179e6ecb46738d
Mateo Galic <mateogalic112@gmail.com>
2021-06-23 13:54:12 +0200

    Installing redux deps.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users table</h1>
+      <table>
+        <tr>
+          <th>Ime</th>
+          <th>Prezime</th>
+          <th>Email</th>
+          <th>Akcije</th>
+        </tr>
+        {/* Users list goes here */}
+      </table>
     </div>
   );
 }
