a5e48ed91aa9d0b177ae13f025f5d7e3498bd4b5
Mateo Galic <mateogalic112@gmail.com>
2021-06-23 12:40:35 +0200

    User model.

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
